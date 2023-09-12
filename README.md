57b3be180aa2f37aeb0393d4f81d403782cd111e
twlight_goose <51844045+twilight-goose@users.noreply.github.com>
2023-09-12 10:27:26 -0400

    basic data reading and conversino functionality

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 This repository exists
 
 ## Environment Creation
+1. Following [these instructions](https://tengkengvang.com/2022/08/24/arcpy-outside-of-arcmap-arcpro/#:~:text=During%20the%20installation%20of%20ArcPy,allow%20ArcPy%20to%20be%20installed.&text=If%20all%20goes%20well%2C%20you,An%20important%20caveat) install Anaconda to gain access to conda, in order to install arcpy locally and import it as a module
+2. Create a conda rooted python channel, in which arcpy can be installed
+3. Create a conda virtual environment with access to the arcpy library
 
+Note: Python is a dependancy of Conda
 
 ## Enironment Setup
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+'''
+Arcpy library is used for data management, analysis, and manipulation. An ArcGIS Pro lisence is required,
+but ideally there is no need to create/load projects to perfrom operations. Everything should be
+contained within these modules, and projects used as ways to check work.
+'''
 import arcpy
+
+
+class DataManager():
+    data_tables = {}
+
+    def __init__(self, *data_path):
+        self.load_all(data_path)
+
+    def load_all(self, path_list):
+        for path in path_list:
+            name = path[path.rindex("\\") + 1:]
+            self.data_tables[name] = arcpy.management.CopyRows(path)
+
+
+def main():
+    aprx = arcpy.mp.ArcGISProject(
+        r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\ArcGIS\Projects\station data explore\station data explore 2.aprx")
+    basinpath = r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\MondayFileGallery\static_attributes.csv"
+    basinoutpath = aprx.defaultGeodatabase + "\\basin_static_table"
+    basintable = arcpy.management.CopyRows(basinpath, basinoutpath)
+    basinpoints = arcpy.management.XYTableToPoint(basinoutpath, aprx.defaultGeodatabase + "\\basin_points_w_attr", "lon", "lat")
+
+
+if __name__ == "__main__":
+    main()
