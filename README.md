a5cf3beaf9ebe60ee4989b5ffb1d71f7ee154c4c
twlight_goose <51844045+twilight-goose@users.noreply.github.com>
2023-09-12 13:35:37 -0400

    Basin and Q C loading, conversion, and plotting

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 This repository exists
 
-## Environment Creation
-1. Following [these instructions](https://tengkengvang.com/2022/08/24/arcpy-outside-of-arcmap-arcpro/#:~:text=During%20the%20installation%20of%20ArcPy,allow%20ArcPy%20to%20be%20installed.&text=If%20all%20goes%20well%2C%20you,An%20important%20caveat) install Anaconda to gain access to conda, in order to install arcpy locally and import it as a module
-2. Create a conda rooted python channel, in which arcpy can be installed
-3. Create a conda virtual environment with access to the arcpy library
+## Environment Creation & Setup
+The setup steps listed below present one way to set up the environment that will enable you to run this code.
+The goal of these steps is to set up a conda channel that has python and arcpy installed, which can be used to run
+the Python scripts.
 
-Note: Python is a dependancy of Conda
+After installing and logging into ArcGIS Pro, follow [these instructions](https://tengkengvang.com/2022/08/24/arcpy-outside-of-arcmap-arcpro/#:~:text=During%20the%20installation%20of%20ArcPy,allow%20ArcPy%20to%20be%20installed.&text=If%20all%20goes%20well%2C%20you,An%20important%20caveat).
+Ensure that you install the arcpy version that corresponds to the arcpy bundled with your installation of ArcGIS Pro.
 
-## Enironment Setup
+## Running the code
+Open the anaconda terminal and navigation to the channel with arcpy installed. Then, using Python, run the file using the file path
--- main.py
+++ main.py
@@ -4,27 +4,80 @@ but ideally there is no need to create/load projects to perfrom operations. Ever
 contained within these modules, and projects used as ways to check work.
 '''
 import arcpy
+import sys
+import os
 
 
-class DataManager():
-    data_tables = {}
-
-    def __init__(self, *data_path):
-        self.load_all(data_path)
-
-    def load_all(self, path_list):
-        for path in path_list:
-            name = path[path.rindex("\\") + 1:]
-            self.data_tables[name] = arcpy.management.CopyRows(path)
-
-
-def main():
+def sample():
     aprx = arcpy.mp.ArcGISProject(
         r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\ArcGIS\Projects\station data explore\station data explore 2.aprx")
     basinpath = r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\MondayFileGallery\static_attributes.csv"
     basinoutpath = aprx.defaultGeodatabase + "\\basin_static_table"
     basintable = arcpy.management.CopyRows(basinpath, basinoutpath)
     basinpoints = arcpy.management.XYTableToPoint(basinoutpath, aprx.defaultGeodatabase + "\\basin_points_w_attr", "lon", "lat")
+
+
+def load_csv(in_path, out_path):
+    arcpy.management.CopyRows(in_path, out_path)
+    f_names = [f.name for f in arcpy.ListFields(out_path)]
+    if "lon" in f_names and "lat" in f_names:
+        arcpy.management.XYTableToPoint(out_path, out_path + "_points", "lon", "lat")
+    return [out_path, out_path + "_points"]
+
+
+def load_sqlite(in_path, out_path):
+    pass
+
+
+def load_data(path, out_path):
+    dir_list = os.listdir(path)
+    data_dict = {}
+
+    for file in dir_list:
+        filename, filetype = os.path.splitext(os.path.basename(file))
+
+        full_path = path + "\\" + file
+        full_out_path = out_path + "\\" + filename
+
+        print("Loading " + file)
+
+        if filetype == ".csv":
+            data_dict[filename] = load_csv(full_path, full_out_path)
+        elif filetype == ".sqlite3":
+            data_dict[filename] = load_sqlite(full_path, full_out_path)
+
+    return data_dict
+
+
+def aprx_map(aprx, data):
+    n = len(aprx.listMaps())
+    newMap = aprx.createMap("Map " + str(n + 1))
+    for key in data.keys():
+        try:
+            newMap.addDataFromPath(data[key][1])
+        except RuntimeError:
+            pass
+        aprx.save()
+
+
+def main():
+    data_path = input("Data Folder Path:\n")
+    aprx_path = input("ArcGIS Project Path:\n")
+
+    if data_path == "":
+        data_path = r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\MondayFileGallery"
+    if aprx_path == "":
+        aprx_path = r"C:\Users\j2557wan\OneDrive - University of Waterloo\Documents\ArcGIS\Projects\station data explore\station data explore 2.aprx"
+
+    aprx = arcpy.mp.ArcGISProject(aprx_path)
+    if os.path.exists(aprx.homeFolder + "\\newGDB.gdb"):
+        arcpy.management.Delete(aprx.homeFolder + "\\newGDB.gdb")
+    arcpy.management.CreateFileGDB(aprx.homeFolder, "newGDB.gdb")
+
+    data = load_data(data_path, aprx.homeFolder + "\\newGDB.gdb")
+
+    aprx_map(aprx, data)
+    aprx.save()
 
 
 if __name__ == "__main__":
