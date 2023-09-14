afc0d114cdb41bbe24bf25dbdc1e7cc176ad8a45
James Wang <j2557wan@uwaterloo.ca>
2023-09-14 16:55:25 -0400

    something

--- Loader.py
+++ Loader.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime
 import pandas as pd
 import geopandas as gpd
@@ -26,27 +27,6 @@ hydat_join_f = "STATION_NUMBER"
 timer = Timer()
 
 
-def get_hydat_station_data():
-    timer.start()
-
-    print("Creating a connection to '{0}'".format(hydat_path))
-    conn = sqlite3.connect(hydat_path)
-
-    table_list = pd.read_sql_query("SELECT * FROM sqlite_master where type= 'table'", conn)
-    station_data = pd.read_sql_query("SELECT * FROM 'STATIONS' WHERE PROV_TERR_STATE_LOC == 'ON'", conn)
-
-    station_data[hydat_join_f] = station_data[hydat_join_f].astype('|S')
-
-    for tbl_name in table_list['tbl_name'][1:]:
-        table_data = pd.read_sql_query("SELECT * FROM %s" % tbl_name, conn)
-        if hydat_join_f in table_data.columns.values:
-            table_data[hydat_join_f] = table_data[hydat_join_f].astype('|S')
-            station_data = station_data.join(table_data, on=[hydat_join_f], lsuffix="_L")
-
-    conn.close()
-    return {"hydat": station_data}
-
-
 def get_monday_files():
     timer.start()
 
@@ -61,7 +41,33 @@ def get_monday_files():
     return data_dict
 
 
-def get_station_data(query: str) -> pd.DataFrame:
+def get_hydat_station_data(query=None):
+    timer.start()
+
+    print("Creating a connection to '{0}'".format(hydat_path))
+    conn = sqlite3.connect(hydat_path)
+
+    table_list = pd.read_sql_query("SELECT * FROM sqlite_master where type= 'table'", conn)
+    station_data = pd.read_sql_query("SELECT * FROM 'STATIONS' WHERE PROV_TERR_STATE_LOC == 'ON'", conn)
+
+    station_data[hydat_join_f] = station_data[hydat_join_f].astype('|S')
+
+    n = 0
+
+    print("Joining station number identified data from other tables")
+    for tbl_name in table_list['tbl_name'][1:]:
+        table_data = pd.read_sql_query("SELECT * FROM %s" % tbl_name, conn)
+        if hydat_join_f in table_data.columns.values:
+            table_data[hydat_join_f] = table_data[hydat_join_f].astype('|S')
+            station_data = station_data.join(table_data, on=[hydat_join_f], lsuffix=str(n))
+            n += 1
+
+    timer.stop()
+    conn.close()
+    return {"hydat": station_data}
+
+
+def get_all_pwqmn_data(query: str) -> pd.DataFrame:
     """
     This function retrieves dated water information of a subset of
     stations based on 'query'
@@ -88,20 +94,32 @@ def get_station_data(query: str) -> pd.DataFrame:
     return df
 
 
-def get_pwqmn_stations():
+def get_pwqmn_station_info(**kwargs):
     """
-    This function reads from the cleaned PWQMN data into RAM using pd, and returns the following information about
-    each station: Name, ID, Longitude, Latitude, and Start Date
+    This function reads from the cleaned PWQMN data using pd, and
+    returns the following information about the set of stations
+    that satisfy requirements passed in kwargs (or all if nothing is
+    passed):
+        - Name
+        - ID
+        - Longitude
+        - Latitude
+        - Start Date
+        - Available variables
     :return:
     """
     timer.start()
     print("Loading PWQMN station info")
 
+    # make it accept kwargs as parameters
+    # loads everything, then sends back a dataframe of just stations that pass the critera
+
     df = pd.read_csv(pwqmn_PATH, usecols=['MonitoringLocationName',
                                           'MonitoringLocationID',
                                           'MonitoringLocationLatitude',
                                           'MonitoringLocationLongitude',
-                                          'ActivityStartDate'])
+                                          'ActivityStartDate',
+                                          'CharacteristicName'])
     grouped = df.groupby(by=['MonitoringLocationName',
                              'MonitoringLocationID',
                              'MonitoringLocationLatitude',
@@ -111,23 +129,24 @@ def get_pwqmn_stations():
 
     # iterate through sequences of (group name, subset object (df)]
     for name, subset_df in grouped:
-        time_range = pd.to_datetime(subset_df['ActivityStartDate'])
-        station_data.append(name + (time_range.min(),))
+        observation_dates = pd.to_datetime(subset_df['ActivityStartDate'])
+        variables = subset_df['CharacteristicName']
+        station_data.append(name + (observation_dates.min(), variables))
 
     # "One-liner" alternative to the lines above
     # station_data =\
     #     [name + (pd.to_datetime(subset_df['ActivityStartDate']).min())
     #      for name, subset_df in grouped]
 
-    station_data = pd.DataFrame(station_data, columns=['Name', 'ID', 'Longitude',
-                                                       'Latitude', 'Start Date'])
+    station_data = pd.DataFrame(station_data, columns=['Name', 'ID', 'Latitude',
+                                                       'Longitude', 'Start Date', 'Variables'])
     station_data.set_index('Name')
 
     timer.stop()
-    return {"pwqmn_stations": station_data}
+    return {"pwqmn": station_data}
 
 
-def load_default() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def load_all() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Loads a set of data at predetermined locations as pandas
     DataFrames
 
@@ -136,7 +155,7 @@ def load_default() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     return {**get_monday_files(),
             **get_hydat_station_data(),
-            **get_pwqmn_stations()}
+            **get_pwqmn_station_info()}
 
 
 def find_xy_fields(df: pd.DataFrame, x: str, y: str) -> [str, str]:
@@ -146,16 +165,17 @@ def find_xy_fields(df: pd.DataFrame, x: str, y: str) -> [str, str]:
     fields = df.columns.values
 
     for field in fields:
-        if any(sub in field.upper() for sub in ["LON", "LONG", "LONGITUDE", "X"]):
-            if x == "":
-                x = field
-            else:
-                x = "Failed"
-        elif any(sub in field.upper() for sub in ["LAT", "LATITUDE", "Y"]):
-            if y == "":
-                y = field
-            else:
-                y = "Failed"
+        if df[field].dtype == float:
+            if field.upper() in ["LON", "LONG", "LONGITUDE", "X"]:
+                if x == "":
+                    x = field
+                else:
+                    x = "Failed"
+            elif field.upper() in ["LAT", "LATITUDE", "Y"]:
+                if y == "":
+                    y = field
+                else:
+                    y = "Failed"
 
     return x, y
 
@@ -182,8 +202,7 @@ def point_gdf_from_df(name_df_pair: dict or pd.DataFrame, x_field="", y_field=""
     else:
         try:
             gdf = gpd.GeoDataFrame(
-                df, geometry=gpd.points_from_xy(df[x], df[y]), crs='epsg:3160'
-            )
+                df.astype(str), geometry=gpd.points_from_xy(df[x], df[y]), crs=4326)
             print("X/Y fields found. Dataframe converted to geopandas point array")
         except KeyError:
             print("X/Y field not found. Operation Failed")
@@ -191,10 +210,30 @@ def point_gdf_from_df(name_df_pair: dict or pd.DataFrame, x_field="", y_field=""
     timer.stop()
 
     if map_result:
-        gdf = gdf.rename(columns={c:str(c) for c in gdf.columns})
         m = gdf.explore()
         outfp = os.path.join(os.path.dirname(__file__), "map.html")
         m.save(outfp)
         webbrowser.open(outfp)
 
     return gdf
+
+
+def map_all(seq: iter):
+    if type(seq) == dict:
+        seq = list(seq.values())
+
+    def explore_recursive(gdfs, n):
+        if n == len(gdfs) - 1:
+            return gdfs[n].explore()
+        else:
+            return gdfs[n].explore(m=explore_recursive(gdfs, n + 1),
+                                   color=hex(random.randrange(0, 2**24)))
+
+    m = explore_recursive(seq, 0)
+    outpath = os.path.join(os.path.dirname(__file__), "map.html")
+    m.save(outpath)
+    webbrowser.open(outpath)
+
+
+def query_df(df, query):
+    return df.query(query)
--- main.py
+++ main.py
@@ -6,17 +6,45 @@ def get_sqlite_table_list(connection):
     pass
 
 
+def map_all(data):
+    point_data = {}
+
+    for key in data.keys():
+        result = Loader.point_gdf_from_df(data[key])
+        if type(result) != int:
+            point_data[key] = result
+
+    Loader.map_all(point_data)
+
+
 def main():
     timer = Loader.Timer()
 
-    monday_files = Loader.get_monday_files()
-    basin_gdf = Loader.point_gdf_from_df(monday_files["basins.csv"], map_result=True)
+    data = Loader.load_all()
+    # map_all(data)
 
-    return
+    run = True
+    while run:
+        print("What dataset would you like to act on? (EXIT to exit)")
+        print("\n".join(data.keys()))
 
-    data = Loader.load_default()
-    for key in data.keys():
-        Loader.display(data[key])
+        dataset = input()
+
+        if dataset not in data.keys():
+            print("Invalid dataset name")
+        elif dataset.upper() == "PWQMN":
+            pwqmn = True
+            while pwqmn:
+                print(data["pwqmn"].dtypes)
+                user_in = input("Type SQL Query (BACK to back):")
+                pwqmn = user_in.upper() != "BACK"
+                try:
+                    print(Loader.query_df(data["pwqmn"], user_in))
+                except ValueError or SyntaxError:
+                    print("Invalid query")
+
+        elif dataset == "EXIT":
+            run = False
 
     timer.stop()
 
