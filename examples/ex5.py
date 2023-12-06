2613438dfe44d3d1e38f4b09dcd30ca504a14acb
Julie <juliane.mai@uwaterloo.ca>
2023-12-06 13:15:37 -0500

    Update ex5.py

--- examples/ex5.py
+++ examples/ex5.py
@@ -17,12 +17,12 @@ def main():
 
     hydat = load_data.get_hydat_stations(bbox=bbox)
     pwqmn = load_data.get_pwqmn_stations(bbox=bbox)
-    
+
     # Load the Onario Hydro Network to use as a river network for matching
     path = os.path.join(load_data.data_path,
                         os.path.join("OHN", "Ontario_Hydro_Network_(OHN)_-_Watercourse.shp"))
     lines = load_data.load_rivers(path=path, bbox=bbox)
-    
+
     # add geographical reference to station data
     hydat = gdf_utils.point_gdf_from_df(hydat)
     pwqmn = gdf_utils.point_gdf_from_df(pwqmn)
@@ -41,7 +41,7 @@ def main():
     print(edge_df.drop(columns='path').to_string())
 
     # plot the matches in an interactive plot
-    browser.match_browser(hydat, pwqmn, network, edge_df, bbox, color='blue')
+    browser.match_browser(hydat, pwqmn, network, edge_df, bbox)
 
 
 if __name__ == "__main__":
